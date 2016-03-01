672e2f79eecd7e03b2ce624d227ed80ba74a4107
WildCerebrus <mathieu.althuser@gmail.com>
2016-03-01 09:22:45 +0100

    add Element
    ajout de la classe Element qui sert de donnée de base

---
+++ src/data/Element.java
@@ -0,0 +1,11 @@
+package data;
+
+public abstract class Element {
+	private String name;
+	public String getName() {
+		return this.name;
+	}
+	public String toString() {
+		return this.name;
+	}
+}
