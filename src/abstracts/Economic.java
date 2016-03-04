5c047dffadbf09c9e1137eec42afd10c7e21aacd
WildCerebrus <mathieu.althuser@gmail.com>
2016-03-04 08:43:11 +0100

    add Corporation constructor

--- src/abstracts/Economic.java
+++ src/abstracts/Economic.java
@@ -11,11 +11,21 @@ public abstract class Economic extends Element {
 	public double getFunds() {
 		return this.funds;
 	}
+	public void setFunds(double f) {
+		this.funds=f;
+	}
 	public ArrayList<AstralObject> getEstates() {
 		return this.estates;
 	}
+	public void setEstates(ArrayList<AstralObject> e) {
+		this.estates=new ArrayList<AstralObject>();
+		this.estates.addAll(e);
+	}
 	public Corporation getCorporation() {
 		return this.corporation;
+	}
+	public void setCorporation(Corporation c) {
+		this.corporation=new Corporation(c);
 	}
 	public String toString() {
 		return super.toString()+" has "+this.funds+" $";
